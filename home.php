fbb3f72618f7a67b8fcafa2d2629fd889495b61d
keshavjatav52 <161579176+keshavjatav52@users.noreply.github.com>
2024-02-28 01:06:44 -0800

    Create home.php

---
+++ home.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        <?php require "style.css"; ?>
+    </style>
+    <title>23070613619 Govt. Higher Sec. School Myana Guna M.P.</title>
+    <link rel="icon" type="image/x-icon" href="icon.png">
+</head>
+<body>
+    <div>
+        <span class="logo">
+            <img src="icon.png" alt="" class="logo">
+        </span>
+        <div class="navbar">
+            <ul>
+                <li>Dashboard</li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    </div>    
+</body>
+</html>
